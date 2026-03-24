1674d71c7112342016a21d4774dbe50c8ed0803e
ShardulJ <Shardul2607@gmail.com>
2026-03-24 00:13:27 -0500

    Update solution for entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -6,6 +6,6 @@ def entropy_node(y):
     """
     _, counts = np.unique(y, return_counts=True)
     p = counts / len(y)
-    # p = p[p > 0]  # filter zero probabilities
+    p = p[p > 0]
     return -np.sum(p * np.log2(p))
     
